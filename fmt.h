19597a960b6361940997b2a10c1b21f9c669c2b3
gmd01bit <gmd01bit@gmail.com>
2024-11-22 13:22:40 +0200

    :sparkles: More types

--- fmt.h
+++ fmt.h
@@ -1,6 +1,7 @@
 #ifndef FMT_H
 #define FMT_H
 
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,14 +21,20 @@
 #define fmt_formatter(ty) \
 	_Generic((ty), \
 	    char: fmt_c, \
+	    uint8_t: fmt_u8, \
 	    uint16_t: fmt_u16, \
 	    uint32_t: fmt_u32, \
 	    uint64_t: fmt_u64, \
+	    int8_t: fmt_i8, \
 	    int16_t: fmt_i16, \
 	    int32_t: fmt_i32, \
 	    int64_t: fmt_i64, \
 	    char *: fmt_str)
 
+#define spec(...) {__VA_ARGS__+0}
+
 #define fmt(ty) fprintf(stdout, fmt_formatter(ty), ty)
+
+#define fmtln fmt((char) '\n')
 
 #endif // !FMT_H
--- test/unittest.c
+++ test/unittest.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #ifndef UNITTEST_MAIN
 #include "unittest.h"
 #include <assert.h>
@@ -7,7 +8,10 @@
 #endif /* ifndef UNITTEST_MAIN */
 
 unittest("format") {
-	puts("-------------------------------------------");
-	fmt("string is printed, then a "), fmt(42U), fmt((char) '&'), fmt(-1);
-	fmt((char) '\n');
+	fmt("-------------------------------------------"), fmtln;
+	fmt("string is printed, then a "), fmt(42U), fmt((char) '&'), fmt(-1),
+	    fmtln;
+	fmt("a byte: "), fmt((uint8_t) 3), fmt(", a signed byte: "), fmt((int8_t) -4), fmtln;
+	fmt("a size_t: "), fmt((size_t) 10), fmtln;
+	fmt("a hex: "), fmt((uint8_t) 123, "x"), fmtln;
 }
